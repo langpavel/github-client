3045f84b9af8c81f49e6dac42f670db03ac8111d
Pavel Lang <langpavel@phpskelet.org>
2018-10-17 02:40:17 +0200

    Rough GitHub client

--- src/App.js
+++ src/App.js
@@ -1,27 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import prettier from 'prettier/standalone';
+import babylon from 'prettier/parser-babylon';
+import GraphiQL from '@langpavel/graphiql';
+import '@langpavel/graphiql/graphiql.css';
 import './App.css';
+
+let ghToken = localStorage.getItem('ghToken');
+
+function setGitHubToken(token) {
+  ghToken = token;
+  localStorage.setItem('ghToken', token);
+  console.log('New token set');
+}
+
+function graphQLFetcher(graphQLParams) {
+  return fetch('https://api.github.com/graphql', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'bearer ' + ghToken,
+    },
+    body: JSON.stringify(graphQLParams),
+  }).then(response => response.json());
+}
+
+GraphiQL.formatResult = response =>
+  prettier.format(JSON.stringify(response), {
+    parser: 'json',
+    plugins: [babylon],
+  });
+
+const CustomTools = () => (
+  <span className="Toolbar-token">
+    <form
+      onSubmit={e => {
+        e.preventDefault();
+        setGitHubToken(e.target.ghToken.value);
+      }}>
+      <label>
+        <a
+          href="https://github.com/settings/tokens"
+          target="_blank"
+          rel="noopener noreferrer">
+          GitHub token
+        </a>
+        {': '}
+        <input name="ghToken" type="password" />
+      </label>
+    </form>
+  </span>
+);
+
+GraphiQL.Toolbar = props => (
+  <div className="toolbar">
+    {props.children}
+    <CustomTools />
+  </div>
+);
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <GraphiQL fetcher={graphQLFetcher} />;
   }
 }
 
