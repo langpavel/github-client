e621446e532dbf7b5c9bcc5f8f12031c45eb0cee
Pavel Lang <langpavel@phpskelet.org>
2018-10-21 05:47:03 +0200

    add api url input

--- src/App.js
+++ src/App.js
@@ -5,24 +5,30 @@ import GraphiQL from '@langpavel/graphiql';
 import '@langpavel/graphiql/graphiql.css';
 import './App.css';
 
-let ghToken = localStorage.getItem('ghToken');
+let apiUrl =
+  localStorage.getItem('graphiql:apiUrl') || 'https://api.github.com/graphql';
+let apiToken =
+  localStorage.getItem('graphiql:token:' + apiUrl) ||
+  localStorage.getItem('ghToken');
 
-function setGitHubToken(token) {
-  ghToken = token;
-  localStorage.setItem('ghToken', token);
-  console.log('New token set');
+function setUrlToken(url, token) {
+  apiUrl = url;
+  apiToken = token;
+  localStorage.setItem('graphiql:apiUrl', url);
+  localStorage.setItem('graphiql:token:' + url, token);
+  console.log('New token set for url', url);
 }
 
-function graphQLFetcher(graphQLParams) {
-  return fetch('https://api.github.com/graphql', {
+const fetcher = graphQLParams => {
+  return fetch(apiUrl, {
     method: 'post',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: 'bearer ' + ghToken,
+      Authorization: 'bearer ' + apiToken,
     },
     body: JSON.stringify(graphQLParams),
   }).then(response => response.json());
-}
+};
 
 GraphiQL.formatResult = response =>
   prettier.format(JSON.stringify(response), {
@@ -30,26 +36,69 @@ GraphiQL.formatResult = response =>
     plugins: [babylon],
   });
 
-const CustomTools = () => (
-  <span className="Toolbar-token">
-    <form
-      onSubmit={e => {
-        e.preventDefault();
-        setGitHubToken(e.target.ghToken.value);
-      }}>
-      <label>
-        <a
-          href="https://github.com/settings/tokens"
-          target="_blank"
-          rel="noopener noreferrer">
-          GitHub token
-        </a>
-        {': '}
-        <input name="ghToken" type="password" />
-      </label>
-    </form>
-  </span>
-);
+class CustomTools extends Component {
+  state = {
+    url: apiUrl,
+    token: apiToken,
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const target = e.target;
+    const url = target.url.value;
+    const token = target.token.value;
+    setUrlToken(e.target.url.value, e.target.token.value);
+    this.setState({ url, token });
+  };
+
+  handleUrlChange = e => {
+    const url = e.target.value;
+    const key = 'graphiql:token:' + url;
+    const token = localStorage.getItem(key) || '';
+    this.setState({ url, token });
+  };
+
+  handleTokenChange = e => {
+    const token = e.target.value;
+    this.setState({ token });
+  };
+
+  render() {
+    return (
+      <span className="Toolbar-token">
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            {'URL: '}
+            <input
+              name="url"
+              type="text"
+              value={this.state.url}
+              autoComplete="graphql-api url"
+              onChange={this.handleUrlChange}
+            />
+          </label>{' '}
+          <label>
+            <a
+              href="https://github.com/settings/tokens"
+              target="_blank"
+              rel="noopener noreferrer">
+              bearer
+            </a>
+            {': '}
+            <input
+              name="token"
+              type="password"
+              onChange={this.handleTokenChange}
+              value={this.state.token}
+              autoComplete="graphql-api-token bearer-token token"
+            />
+          </label>{' '}
+          <button type="submit">Set</button>
+        </form>
+      </span>
+    );
+  }
+}
 
 GraphiQL.Toolbar = props => (
   <div className="toolbar">
@@ -60,7 +109,7 @@ GraphiQL.Toolbar = props => (
 
 class App extends Component {
   render() {
-    return <GraphiQL fetcher={graphQLFetcher} />;
+    return <GraphiQL fetcher={fetcher} />;
   }
 }
 
